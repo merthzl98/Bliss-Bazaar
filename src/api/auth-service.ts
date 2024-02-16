2a9ffb7ebd9910ab1c4c87ef92d2fa5d9dead96c
chopsuey123 <merthzl98@gmail.com>
2024-02-16 18:58:40 +0300

    Update Auth Request

--- src/api/auth-service.ts
+++ src/api/auth-service.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const getToken = async (email: string, password: string) => {
   return await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDjiQI3EZapPS48nMHOPNwp3H6wnWGiIKM",
+    `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${
+      import.meta.env.VITE_FIREBASE_API_KEY
+    }`,
     { email: email, password: password, returnSecureToken: true }
   );
 };
